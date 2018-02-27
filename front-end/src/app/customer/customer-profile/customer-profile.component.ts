430cef66167e38d2f05be537baabf83f58f1b638
Bruno Campos <hello.bruno.campos@gmail.com>
2018-02-26 20:41:46 -0500

    Create the component for the customer profile

---
+++ front-end/src/app/customer/customer-profile/customer-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-profile',
+  templateUrl: './customer-profile.component.html',
+  styleUrls: ['./customer-profile.component.scss']
+})
+export class CustomerProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
