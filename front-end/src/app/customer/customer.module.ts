42098dd8479bb01f7e8c9d6172ea810b29bce427
Bruno Campos <hello.bruno.campos@gmail.com>
2018-02-26 20:41:14 -0500

    Add content for the customer profile

--- front-end/src/app/customer/customer.module.ts
+++ front-end/src/app/customer/customer.module.ts
@@ -4,12 +4,13 @@ import { CommonModule } from '@angular/common';
 import { CustomerRoutingModule } from './customer-routing.module';
 import { CustomerHomeComponent } from './customer-home/customer-home.component';
 import { CustomerLayoutComponent } from './customer-layout/customer-layout.component';
+import { CustomerProfileComponent } from './customer-profile/customer-profile.component';
 
 @NgModule({
 	imports: [
 		CommonModule,
 		CustomerRoutingModule
 	],
-	declarations: [CustomerHomeComponent, CustomerLayoutComponent]
+	declarations: [CustomerHomeComponent, CustomerLayoutComponent, CustomerProfileComponent]
 })
 export class CustomerModule { }
