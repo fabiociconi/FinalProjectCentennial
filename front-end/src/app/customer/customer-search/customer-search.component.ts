4fdd11b494fbae53eeb29d0ea96b92fd46dd0717
Bruno Campos <hello.bruno.campos@gmail.com>
2018-04-14 17:06:32 -0400

    Create search module

---
+++ front-end/src/app/customer/customer-search/customer-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-search',
+  templateUrl: './customer-search.component.html',
+  styleUrls: ['./customer-search.component.scss']
+})
+export class CustomerSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
